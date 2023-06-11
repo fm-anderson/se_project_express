95f0673b927f7e4b1e8090acb58acddf6697261f
AndersonM <andersonmarquesweb@gmail.com>
2023-06-11 19:22:28 -0400

    schema created: user and clothingItem

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -5,18 +5,37 @@ const clothingItem = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    minlength: 2,
+    maxlength: 30,
   },
   weather: {
     type: String,
     required: true,
+    enum: ["hot", "cold", "warm"],
   },
-  imageURL: {
+  imageUrl: {
     type: String,
     required: true,
-    validator: {
+    validate: {
       validator: (v) => validator.isURL(v),
-      message: "Link is not valid",
+      message: "You must enter a valid URL",
     },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: [],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
 
