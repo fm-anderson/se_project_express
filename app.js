737a38b01b0e0b2ee89801f9b32831d4054437cb
AndersonM <andersonmarquesweb@gmail.com>
2023-06-11 11:00:26 -0400

    feature: mongodb setup

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const { PORT = 3001 } = process.env;
 const app = express();
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/wtwr_db",
+  (res) => console.log("connected to DB", res),
+  (err) => console.log("DB error", err)
+);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
