6dae5e027166fa3e69773a6ec246732c2bb85fda
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2023-06-20 18:08:51 -0400

    user schema update: email and password added

--- models/user.js
+++ models/user.js
@@ -4,17 +4,34 @@ const validator = require("validator");
 const User = new mongoose.Schema({
   name: {
     type: String,
+    default: "Elise Bouer",
     required: true,
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
+    default:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Elise.png",
     required: true,
     validate: {
       validator: (v) => validator.isURL(v),
       message: "You must enter a valid URL",
     },
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: "Wrong email format",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
   },
 });
 
