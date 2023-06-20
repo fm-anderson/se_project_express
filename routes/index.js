57444958610b67dc578659e7cc6eaac55bb45e6c
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2023-06-20 18:45:46 -0400

    feature: sign up and sign in routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 const clothingItem = require("./clothingItems");
 const user = require("./users");
 const { NOTFOUND_ERROR } = require("../utils/error");
+const { createUser, login } = require("../controllers/users");
 
 router.use("/items", clothingItem);
 router.use("/users", user);
+router.post("/signup", createUser);
+router.post("/signin", login);
 
 router.use((req, res) => {
   res.status(NOTFOUND_ERROR.error).send({ message: "Router not found" });
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,3 @@
 const router = require("express").Router();
 
-const { createUser, getUsers, getUser } = require("../controllers/users");
-
-router.post("/", createUser);
-
-router.get("/", getUsers);
-router.get("/:userId", getUser);
-
 module.exports = router;
