42c14a5af38da0a07c67b9fdb3e5d3aa5e92d0b0
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2023-06-30 23:51:02 -0400

    feature: middleware validation

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const { errors } = require("celebrate");
 const errorHandler = require("./middlewares/errorHandler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -15,9 +17,13 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
+app.use(requestLogger);
+
 app.use(routes);
 
-app.use(errorHandler);
+app.use(errorLogger);
+app.use(errors()); // celebrate error handler
+app.use(errorHandler); // local centralized error handler
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
